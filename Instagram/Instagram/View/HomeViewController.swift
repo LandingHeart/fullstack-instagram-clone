379f3bb3ac49cc093e40eb87e3f4cf5a3fdb264e
Huang Yan <88303850+steven-yanh@users.noreply.github.com>
2022-10-17 20:40:05 -0400

    iOS development  (#4)
    * inital commit
    
    * add: tabBar VC, app icon and lauch screen
    
    * add: missing unstaged change

---
+++ Instagram/Instagram/View/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  HomeViewController.swift
+//  Instagram
+//
+//  Created by Huang Yan on 10/15/22.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Instagram/Instagram/View/VideoViewController.swift
@@ -0,0 +1,19 @@
+//
+//  VideoViewController.swift
+//  Instagram
+//
+//  Created by Huang Yan on 10/17/22.
+//
+
+import UIKit
+
+class VideoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemRed
+    }
+    
+
+
+}
