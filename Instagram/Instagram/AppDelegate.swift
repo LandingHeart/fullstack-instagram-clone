a3dceb131c71fa1a280d002bd27e7cc1a3ac7d25
steven-yanh <hyan0820@gmail.com>
2022-10-17 20:19:25 -0400

    add: missing unstaged change

--- Instagram/Instagram/AppDelegate.swift
+++ Instagram/Instagram/AppDelegate.swift
@@ -10,15 +10,16 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
     var window: UIWindow?
+    
+    let mainVC = MainViewController()
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.makeKeyAndVisible()
-        window?.backgroundColor = .systemBlue
-        window?.rootViewController = ViewController()
+        window?.backgroundColor = .systemBackground
+        window?.rootViewController = mainVC
         
         return true
     }
--- Instagram/Instagram/View/HomeViewController.swift
+++ Instagram/Instagram/View/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  Instagram
 //
 //  Created by Huang Yan on 10/15/22.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
