a22a2af2fc6cfcd5d792b2f11619a384a3010614
Shinan Liu <land.liu0123@gmail.com>
2022-10-19 11:15:26 -0400

    commented out assoication of table (#6)

--- backend/services/PostService.js
+++ backend/services/PostService.js
@@ -3,9 +3,8 @@ const Post = require("../models/index").post;
 module.exports = class PostService {
   static async findAll() {
     try {
-      // const allPost = await Post.findAll();
-      console.log(Post);
-      // return allPost;
+      const allPost = await Post.findAll();
+      return allPost;
     } catch (error) {
       console.log(`could not find all post${error}`);
     }
