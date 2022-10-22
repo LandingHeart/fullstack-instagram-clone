50587c289dfb1f67b6e05c6eb08ad405ffcd6d69
steven-yanh <hyan0820@gmail.com>
2022-10-22 01:16:23 -0400

    add: post header

--- Instagram/Instagram/View/MainControllers/MainViewController.swift
+++ Instagram/Instagram/View/MainControllers/MainViewController.swift
@@ -8,7 +8,15 @@
 import UIKit
 class MainViewController: UITabBarController {
     
-    let barHeight: CGFloat = 88
+    let homeNC = UINavigationController(rootViewController: HomeViewController())
+    
+    let searchNC = SearchViewController()
+    
+    let videoNC = UINavigationController(rootViewController: VideoViewController())
+    
+    let shopNC = UINavigationController(rootViewController: ShopViewController())
+    
+    let profileNC = UINavigationController(rootViewController: ProfileViewController())
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,32 +24,36 @@ class MainViewController: UITabBarController {
         setupBars()
     }
     override func viewDidLayoutSubviews() {
+        let barHeight: CGFloat = 88
         super.viewDidLayoutSubviews()
         tabBar.frame.size.height = barHeight
         tabBar.frame.origin.y = view.frame.height - barHeight
     }
     private func setupViews() {
         
-        let homeNC = UINavigationController(rootViewController: HomeViewController())
         homeNC.tabBarItem = UITabBarItem(title: nil, image: UIImage(systemName: "house"), selectedImage: UIImage(systemName: "house.fill"))
         
-        let searchNC = SearchViewController()
         searchNC.setTabBarImage(systemName: "magnifyingglass")
         
-        let videoNC = UINavigationController(rootViewController: VideoViewController())
         videoNC.tabBarItem = UITabBarItem(title: nil, image: UIImage(systemName: "video"), selectedImage: UIImage(systemName: "video.fill"))
         
-        let shopNC = UINavigationController(rootViewController: ShopViewController())
         shopNC.tabBarItem = UITabBarItem(title: nil, image: UIImage(systemName: "bag"), selectedImage: UIImage(systemName: "bag.fill"))
         
-        let profileNC = UINavigationController(rootViewController: ProfileViewController())
         profileNC.tabBarItem = UITabBarItem(title: nil, image: UIImage(systemName: "person.circle"), selectedImage: UIImage(systemName: "person.circle.fill"))
         
         viewControllers = [homeNC,searchNC,videoNC,shopNC,profileNC]
         
     }
     private func setupBars() {
+        homeNC.navigationBar.tintColor = .systemBackground
+        
+        videoNC.navigationBar.tintColor = .systemBackground
+        
+        shopNC.navigationBar.tintColor = .systemBackground
+        
+        profileNC.navigationBar.tintColor = .systemBackground
+        
         tabBar.backgroundColor = .systemBackground
-        tabBar.tintColor = .black
+        tabBar.tintColor = .systemBackground
     }
 }
