9fc298328a7467b0fdd284cb3aab0f3b3c26a839
Xiaoqian He <xiaoqianhe@Xiaoqians-MBP.fios-router.home>
2022-11-05 22:32:08 -0400

    Home Icon

---
+++ frontend/src/components/HomeIcon.js
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import HomeoutLinedIcon from "@mui/icons-material/HomeOutlined";
+import PostModal from "./PostModal";
+import HomeOutlined from "@mui/icons-material/HomeOutlined";
+
+export default function HomeIcon(){
+
+    return(<HomeOutlined/>)
+
+
+}
