a9c1243a2bba73a47668de082e8b0eb9f74f9c75
ShawnHXQ <51977245+ShawnHXQ@users.noreply.github.com>
2022-11-29 13:30:11 -0500

    Discover icon (#18)
    * Home Icon
    
    * Add Home Icon
    
    * Remove home function
    
    * Home icon
    
    * Discover Icon
    
    Co-authored-by: Xiaoqian He <xiaoqianhe@Xiaoqians-MBP.fios-router.home>

---
+++ frontend/src/components/DiscoverIcon.js
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import PostModal from "./PostModal";
+import DiscoverIcon from "@mui/icons-material/SearchOutlined"
+import { Button } from "@mui/material";
+import React, { Component } from 'react';
+
+export default function discoverIcon(){
+    return(
+            <DiscoverIcon/>
+        )
+}
