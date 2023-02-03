6f6c00b59f7aebcc041cb154e1d2f1b9e49f3710
Shinan Liu <land.liu0123@gmail.com>
2023-02-03 17:32:52 -0500

    change url

--- views/src/api.js
+++ views/src/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const url = "https//localhost:8080";
+// const url = "https//localhost:8080";
+const url = "fullstack-instagram-clone.herokuapp.com";
 // const url = "/";
 const api = axios.create({
   baseURL: url,
