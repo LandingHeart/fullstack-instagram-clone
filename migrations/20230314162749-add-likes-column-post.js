43b777fccb6a1ed4a007ace26a1b22c387a1ffdd
steven-yanh <hyan0820@gmail.com>
2023-03-14 12:53:49 -0400

    add likes, dislikes column to post model

---
+++ migrations/20230314162749-add-likes-column-post.js
@@ -0,0 +1,29 @@
+"use strict";
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    return Promise.all([
+      queryInterface.addColumn(
+        "posts", // table name
+        "likes", // new field name
+        {
+          type: Sequelize.DataTypes.INTEGER,
+          defaultValue: 0,
+          allowNull: false,
+        }
+      ),
+      queryInterface.addColumn("posts", "dislikes", {
+        type: Sequelize.DataTypes.INTEGER,
+        defaultValue: 0,
+        allowNull: false,
+      }),
+    ]);
+  },
+
+  async down(queryInterface, Sequelize) {
+    return Promise.all([
+      queryInterface.removeColumn("posts", "likes"),
+      queryInterface.removeColumn("posts", "dislikes"),
+    ]);
+  },
+};
